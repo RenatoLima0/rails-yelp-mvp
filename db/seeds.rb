587ee6a43152f3c0990acd5fa6fae9b4f43de34c
Renato Lima <renatoslima46@gmail.com>
2020-05-27 14:25:23 -0300

    Models e Route ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,43 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  restaurant1 = Restaurant.create([
+    { name:  Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: 'chinese'
+    }
+  ])
+
+  restaurant2 = Restaurant.create([
+    { name:  Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: 'italian'
+    }
+  ])
+
+  restaurant3 = Restaurant.create([
+    { name:  Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: 'japanese'
+    }
+  ])
+
+  restaurant4 = Restaurant.create([
+    { name:  Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: 'french'
+    }
+  ])
+
+  restaurant5 = Restaurant.create([
+    { name:  Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: 'belgian'
+    }
+  ])
+  # Character.create(name: 'Luke', movie: movies.first)
