3f182b70df821cf37a348e08b659812b94b50571
Renato Lima <renatoslima46@gmail.com>
2020-05-27 15:16:48 -0300

    Complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,42 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Deleted'
+  end
+
   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end 
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
